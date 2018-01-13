228080950c10055f9ad226141c68945c505807b4
SO YOUNG <LEESOYOUNG@itui-macbook-pro.local>
2018-01-13 14:57:49 +0900

    son

--- JsonParserBasic/ViewController.swift
+++ JsonParserBasic/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
+        let jsonString = "{\"name\":\"soyoung\",\"weight\":65}"
+        guard let jsonData = jsonString.data(using: .utf8) else {return}
+        
+        do {
+            if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                print(json["name"] ?? "")
+                print(json["weight"] ?? "")
+            }
+        }
+        catch let error {
+            print("parsing error\(error)")
+        }
     }
 
 
